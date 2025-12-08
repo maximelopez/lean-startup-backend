2b2414432014c4091574beaee49655b2403b29e3
Maxime <maxime.lpz@outlook.fr>
2025-12-08 14:01:13 +0100

    vercel.json

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Express app!');
+app.get('/', (req, res) => res.send('Express app!'));
+
+// Connexion MongoDB
+connectDB().catch(error => {
+  console.error('❌ Erreur connexion MongoDB :', error);
 });
+
+// Pour le dev local uniquement
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+}
 
 export default app;

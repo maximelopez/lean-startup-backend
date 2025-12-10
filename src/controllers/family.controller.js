dd682c1180d79353bf95a1ea31cebb5c6017ec3c
Trougier <theo.rgr2404@gmail.com>
2025-12-10 10:13:58 +0100

    controller et service families

--- src/controllers/family.controller.js
+++ src/controllers/family.controller.js
@@ -1,26 +1,80 @@
 import * as familyService from '../services/family.service.js';
 
 export const createFamily = async (req, res) => {
-    try {
-        const family = await familyService.createFamily(req.body);
-        res.status(201).json(family);
-    } catch (error) {
-        res.status(400).json({ message: 'Impossible de créer la famille.', error: error.message });
-    }
+  try {
+    const family = await familyService.createFamily(req.body);
+    res.status(201).json(family);
+  } catch (error) {
+    res.status(400).json({ message: 'Impossible de créer la famille.', error: error.message });
+  }
 };
 
 export const getAllFamilies = async (req, res) => {
-
+  try {
+    const families = await familyService.getAllFamilies();
+    return res.status(200).json(families);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Erreur lors de la récupération des familles",
+      error: error.message
+    });
+  }
 };
 
-export const getFamily = async (req, res) => {
-
+export const getFamilyById = async (req, res) => {
+  try {
+    const family = await familyService.getFamilyById(req.params.id);
+    res.status(200).json(family);
+  } catch (error) {
+    res.status(404).json({
+      message: "Famille non trouvée.",
+      error: error.message
+    });
+  }
 };
 
 export const updateFamily = async (req, res) => {
+  try {
+    const updated = await familyService.updateFamily(req.params.id, req.body);
 
+    return res.status(200).json({
+      message: "Famille mise à jour avec succès",
+      family: updated
+    });
+
+  } catch (error) {
+
+    // ID invalide → CastError
+    if (error.name === "CastError") {
+      return res.status(400).json({ message: "ID invalide" });
+    }
+
+    return res.status(404).json({
+      message: "Famille non trouvée",
+      error: error.message
+    });
+  }
 };
 
 export const deleteFamily = async (req, res) => {
+  try {
+    const deleted = await familyService.deleteFamily(req.params.id);
 
+    return res.status(200).json({
+      message: "Famille supprimée avec succès",
+      deleted
+    });
+
+  } catch (error) {
+
+    // ID invalide
+    if (error.name === "CastError") {
+      return res.status(400).json({ message: "ID invalide" });
+    }
+
+    return res.status(404).json({
+      message: "Famille non trouvée.",
+      error: error.message
+    });
+  }
 };
--- src/services/family.service.js
+++ src/services/family.service.js
@@ -1,6 +1,35 @@
 import { Family } from "../models/family.model.js";
 
+  
 export const createFamily = async (data) => {
      const family = new Family(data);
      return await family.save();
+};
+
+
+export const getAllFamilies = async () => {
+     return await Family.find();
+}
+
+export const getFamilyById = async (id) => {
+     const family = await Family.findById(id);
+     return family;
+};
+
+export const updateFamily = async (id, updateData) => {
+     const updated = await Family.findByIdAndUpdate(
+     id,
+     updateData,
+     { new: true, runValidators: true }
+);
+
+if (!updated) {
+     throw new Error("Famille non trouvée");
+}
+return updated;
+};
+
+
+export const deleteFamily = async (id) => {
+     return await Family.findByIdAndDelete(id);
 };
